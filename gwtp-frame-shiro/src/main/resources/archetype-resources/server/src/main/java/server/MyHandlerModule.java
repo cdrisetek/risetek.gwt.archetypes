b48bbc506cb619887e9d28ded487077811c07873
wangyc@risetek.com <wangyc@risetek.com>
2020-10-06 18:19:15 +0800

    BatchAction implemented IsSerializable

--- gwtp-frame-shiro/src/main/resources/archetype-resources/server/src/main/java/server/MyHandlerModule.java
+++ gwtp-frame-shiro/src/main/resources/archetype-resources/server/src/main/java/server/MyHandlerModule.java
@@ -2,14 +2,16 @@ package ${package}.server;
 
 import com.gwtplatform.dispatch.rpc.server.guice.HandlerModule;
 import ${package}.server.bindery.AutoLoadModule;
-import ${package}.share.SecurityAction;
+import ${package}.share.UnsecuredSerializableBatchAction;
 import ${package}.share.container.StateAction;
 
 @AutoLoadModule
 public class MyHandlerModule extends HandlerModule {
 	@Override
 	protected void configureHandlers() {
-		bindHandler(SecurityAction.class, SecurityActionHandler.class);
 		bindHandler(StateAction.class, StateActionHandler.class);
+
+		// Binder common UnsecuredSerializableBatchAction and it Handler
+		bindHandler(UnsecuredSerializableBatchAction.class, SerializableBatchActionHandler.class);
 	}
 }
