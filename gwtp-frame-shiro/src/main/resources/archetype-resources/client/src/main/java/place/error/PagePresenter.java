96df63cfac45051ef744a1474ca405b2b43dbd9a
wang <wangyuchun1969@outlook.com>
2022-10-31 15:22:17 +0800

    add error place reveal method by fire event

--- gwtp-frame-shiro/src/main/resources/archetype-resources/client/src/main/java/place/error/PagePresenter.java
+++ gwtp-frame-shiro/src/main/resources/archetype-resources/client/src/main/java/place/error/PagePresenter.java
@@ -1,5 +1,6 @@
 package ${package}.place.error;
 
+import com.google.gwt.core.client.GWT;
 import com.google.inject.Inject;
 import com.google.web.bindery.event.shared.EventBus;
 import com.gwtplatform.mvp.client.HasUiHandlers;
@@ -8,26 +9,49 @@ import com.gwtplatform.mvp.client.View;
 import com.gwtplatform.mvp.client.annotations.NameToken;
 import com.gwtplatform.mvp.client.annotations.NoGatekeeper;
 import com.gwtplatform.mvp.client.annotations.ProxyCodeSplit;
+import com.gwtplatform.mvp.client.annotations.ProxyEvent;
+import com.gwtplatform.mvp.client.proxy.PlaceManager;
 import com.gwtplatform.mvp.client.proxy.ProxyPlace;
+import com.gwtplatform.mvp.shared.proxy.PlaceRequest;
 import ${package}.NameTokens;
 import ${package}.place.root.RootPresenter;
 
 public class PagePresenter extends
 		Presenter<PagePresenter.MyView, PagePresenter.MyProxy>
-		implements MyUiHandlers {
+		implements MyUiHandlers, RevealErrorEventHandler {
 	
 	public interface MyView extends View, HasUiHandlers<MyUiHandlers> {
+		void setErrorMessage(String message);
 	}
 
 	@ProxyCodeSplit
 	@NameToken(NameTokens.error)
     @NoGatekeeper
 	public interface MyProxy extends ProxyPlace<PagePresenter> {}
-	
+	private final PlaceManager placeManager;
 	@Inject
 	public PagePresenter(final EventBus eventBus, final MyView view,
+			final PlaceManager placeManager,
 			final MyProxy proxy) {
 		super(eventBus, view, proxy, RootPresenter.SLOT_MainContent);
+		this.placeManager = placeManager;
 		getView().setUiHandlers(this);
+	}
+	
+	@Override
+	public void onReveal() {
+		PlaceRequest request = placeManager.getCurrentPlaceRequest();
+		String message = request.getParameter("message", null);
+		if(null != message) {
+			GWT.log("error message:" + message);
+			getView().setErrorMessage(message);
+		}
+	}
+
+	@ProxyEvent
+	@Override
+	public void onRevealErrorPlace(RevealErrorEvent event) {
+		getView().setErrorMessage(event.exceptionName);
+		forceReveal();
 	}
 }
